d79113e33a66d8117be2a34efe7c4a4ed76f9c42
Chris McKnight <cmckni3@gmail.com>
2017-02-09 15:36:20 -0600

    refactor(migrations): Cleanup devise migration

--- db/migrate/20160808013159_create_blogs.rb
+++ db/migrate/20160808013159_create_blogs.rb
@@ -1,8 +1,7 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
-      t.string :title, null: false
-      t.string :slug, null: false
+      [:title, :slug].each { |column| t.string column, null: false }
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
